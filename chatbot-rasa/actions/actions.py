f45801460cda88fea7b1b36371108a78239627b5
ryzust <Zrubiol1700@alumno.ipn.mx>
2022-02-28 23:21:14 -0600

    añadido el cuestionario de tipo de aprendizaje y endpoints

--- chatbot-rasa/actions/actions.py
+++ chatbot-rasa/actions/actions.py
@@ -1,27 +1,72 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Text, List, Optional, Dict, Any
+from rasa_sdk.forms import FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk import Tracker, Action
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+def calcular_estilo(respuestas):
+    puntajes = [0, 0, 0]  # visual, auditivo, kinestesico
+    for i in range(0, 5):
+        puntajes[0] = puntajes[0] + respuestas[i]
+    for i in range(5, 10):
+        puntajes[1] = puntajes[1] + respuestas[i]
+    for i in range(10, 15):
+        puntajes[2] = puntajes[2] + respuestas[i]
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    max_value = max(puntajes)
+    estilo = ""
+    for i in range(0, 3):
+        if puntajes[i] == max_value:
+            if i == 0:
+                estilo = estilo + "Visual"
+            if i == 2:
+                estilo = estilo + " Auditivo"
+            if i == 0:
+                estilo = estilo + " Kinestesico"
+    return estilo
+
+
+class SubmitEstiloForm(Action):
+    def name(self) -> Text:
+        return "submit_estilo_form"
+
+    async def run(
+            self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        respuestas = []
+        r1 = int(tracker.get_slot("r1"))
+        r2 = int(tracker.get_slot("r2"))
+        r3 = int(tracker.get_slot("r3"))
+        r4 = int(tracker.get_slot("r4"))
+        r5 = int(tracker.get_slot("r5"))
+        r6 = int(tracker.get_slot("r6"))
+        r7 = int(tracker.get_slot("r7"))
+        r8 = int(tracker.get_slot("r8"))
+        r9 = int(tracker.get_slot("r9"))
+        r10 = int(tracker.get_slot("r10"))
+        r11 = int(tracker.get_slot("r11"))
+        r12 = int(tracker.get_slot("r12"))
+        r13 = int(tracker.get_slot("r13"))
+        r14 = int(tracker.get_slot("r14"))
+        r15 = int(tracker.get_slot("r15"))
+        respuestas.append(r1)
+        respuestas.append(r2)
+        respuestas.append(r3)
+        respuestas.append(r4)
+        respuestas.append(r5)
+        respuestas.append(r6)
+        respuestas.append(r7)
+        respuestas.append(r8)
+        respuestas.append(r9)
+        respuestas.append(r10)
+        respuestas.append(r11)
+        respuestas.append(r12)
+        respuestas.append(r13)
+        respuestas.append(r14)
+        respuestas.append(r15)
+
+        estilo = calcular_estilo(respuestas)
+
+        dispatcher.utter_message("Tu estilo es: ", estilo)
+
+        return []
